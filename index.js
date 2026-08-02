11de9b56562427d4aaa20ca2486f66297901b3a8
Christian <trnchristian94@gmail.com>
2026-08-02 16:19:42 +0000

    auto deployment commands

--- index.js
+++ index.js
@@ -58,4 +58,16 @@ client.on(Events.InteractionCreate, async interaction => {
 
 });
 
+// Registrar comandos al arrancar
+const { REST, Routes } = require("discord.js");
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const cmds = [...client.commands.values()].map(c => c.data.toJSON());
+
+rest.put(
+    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    { body: cmds }
+).then(() => console.log("✅ Comandos registrados."));
+
+client.login(process.env.TOKEN);
+
 client.login(process.env.TOKEN);
